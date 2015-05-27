23a58ec709a2475042b7b0bcb3957b6d08e73fdb
Paul <pribierre@gmail.com>
2015-05-27 09:27:47 +0200

    Ajout controller recette

---
+++ src/RecipeControlerBean.java
@@ -0,0 +1,26 @@
+package processing;
+
+@ManagedBean
+@ApplicationScoped
+public class RecipeControlerBean {
+
+    private RecipesDao recipeDao;
+    public RecipeControlerBean() {
+        this.recipeDao = DaoFabric.getInstance().createRecipesDao();
+    }
+
+    public void loadAllRecipe(){
+
+        ArrayList<RecipeModel> list = this.recipeDao.getAllRecipes();
+
+        RecipeListModelBean recipeList = new RecipeListModelBean();
+        for(RecipeModel recipe:list){
+            recipeList.addRecipeList(recipe);
+        }
+//récupère l'espace de mémoire de JSF
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        Map<String, Object> sessionMap = externalContext.getSessionMap();
+//place la liste de recette dans l'espace de mémoire de JSF
+        sessionMap.put("recipeList" , recipeList);
+    }
+}
