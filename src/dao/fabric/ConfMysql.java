143085fc9ebf9a77fcc5a508f5d3ad7fb1b75421
Paul <pribierre@gmail.com>
2015-05-27 13:56:29 +0200

    GitIgnor

---
+++ src/dao/fabric/ConfMysql.java
@@ -0,0 +1,12 @@
+package dao.fabric;
+
+/**
+ * Created by Paul on 27/05/2015.
+ */
+public class ConfMysql {
+    public static String DB_HOST = "localhost";
+    public static String DB_PORT = "49916";
+    public static String DB_NAME = "themagicpanbd";
+    public static String DB_USER = "root";
+    public static String DB_PWD = "";
+}
